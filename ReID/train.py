cd9d46857327a94fa9cb89e0a4c3dc30d728b161
jackie840129 <jackieliu@media.ee.ntu.edu.tw>
2018-01-24 15:44:58 +0800

    alter train

--- ReID/train.py
+++ ReID/train.py
@@ -1,11 +1,83 @@
 import torch
-from utils import V_Re_ID_Dataset
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+from utils import V_Re_ID_Dataset,Get_DataLoader
 import models
+import sys
+from tqdm import tqdm
+import argparse
 import sys
 
 
-path = sys.argv[1]
 
-Dataset = V_Re_ID_Dataset(path)
-print(len(Dataset))
+def train(args,Dataset,Dataloader,net):
+
+    optimizer = optim.Adam(net.parameters(),lr=args.lr)
+    criterion = nn.CrossEntropyLoss()
+
+    for e in range(args.n_epochs):
+        
+        pbar = tqdm(total=len(Dataset),ncols=100,leave=True)
+        pbar.set_description('Epoch %d'%(e))
+
+        epoch_loss = 0
+        iter_count = 0
+        for i_batch,samples in enumerate(Dataloader):
+            iter_count +=1
+            b_img = Variable(samples['img']).cuda()
+            b_gt = Variable(samples['gt'].squeeze(1)).cuda()
+            b_size = b_img.size(0)
+            net.zero_grad()
+            #forward
+            b_pred = net(b_img)
+            loss = criterion(b_pred,b_gt)
+            epoch_loss += loss.data[0]
+            # backward
+            loss.backward()
+
+            optimizer.step()
+            pbar.update(b_size)
+            pbar.set_postfix({'loss':'%.2f'%(loss.data[0])})
+        pbar.close()
+        print('Training total loss = %.3f'%(epoch_loss/iter_count))
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train Re-ID net')
+    parser.add_argument('--info',default='train_info.txt',help='txt file contain path of data')
+    parser.add_argument('--crop',type=bool,default=True,help='Whether crop the images')
+    parser.add_argument('--flip',type=bool,default=True,help='Whether randomly flip the image')
+    parser.add_argument('--pretrain',type=bool,default=True,help='Whether use pretrained model')
+    parser.add_argument('--lr',type=float,default=0.001,help='learning rate')
+    parser.add_argument('--batch_size',type=int,default=1,help='batch size number')
+    parser.add_argument('--n_epochs',type=int,default=50,help='number of training epochs')
+    parser.add_argument('--load_ckpt',default=None,help='path to load ckpt')
+
+    args = parser.parse_args()
+
+    ## Get Dataset & DataLoader
+    Dataset = V_Re_ID_Dataset(args.info,crop=args.crop,flip=args.flip,pretrained_model=args.pretrain)
+    Dataloader = Get_DataLoader(Dataset,batch_size=args.batch_size)
+
+    ## get Model
+    net = models.ResNet(Dataset.n_id,n_layers=50,pretrained=args.pretrain)
+    if torch.cuda.is_available():
+        net.cuda()
+    
+    if args.load_ckpt == None:
+        print(len(Dataset))
+        train(args,Dataset,Dataloader,net)
+
+
+
+
+
 
--- ReID/utils.py
+++ ReID/utils.py
@@ -13,69 +13,58 @@ import models
 
 class V_Re_ID_Dataset(Dataset):
     def __init__(self, txt_file,resize=(224,224),crop=False,flip=False,pretrained_model=True):
-        self.t_resize = transforms.Resize(resize)
-        if crop == True:
-            self.t_b_resize = transforms.Resize((resize[0]+50,resize[1]+50))
-            self.t_crop = transforms.RandomCrop(resize)
-            self.n_crop = 3
-        if flip == True:
-            self.t_flip = transforms.RandomHorizontalFlip()
-        if pretrained_model == True:
-            self.t_norm = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                              std=[0.229, 0.224, 0.225])
-        self.to_tensor = transforms.ToTensor()
-        self.data,self.label = self.load_img_file(txt_file,resize,flip,crop,pretrained_model)
-    
-    def load_img_file(self,txt_file,resize,flip,crop,pre):
-        print('------Start loading img files in "%s"------'%(txt_file))
-        print('-- 1. Resize img to %s'%(resize,))
-        print('-- 2. Crop : %r  , Flip : %r'%(crop,flip))
-        t0 = time.time()
-        img_list = []
-        label_list = []
+        ######################
+        self.img_list = []
+        self.label_list = []
         file = open(txt_file,'r')
-        for line in file:
-            img_name,label = line.strip().split(' ')
-            img = Image.open(img_name)
-            if img.size[0]<50 or img.size[1]<50:
-                continue
-            ## Random Flip
-            if flip == True:
-                img = self.t_flip(img)
-            if resize != None:
-                if pre == True:
-                    tmp_img = self.t_norm(self.to_tensor(self.t_resize(img)))
-                else:
-                    tmp_img = self.to_tensor(self.t_resize(img))
-                img_list.append(tmp_img)
-                label_list.append(int(label))
-            if crop == True:
-                for i in range(self.n_crop):
-                    if pre == True:
-                        tmp_img = self.t_norm(self.to_tensor(self.t_crop(self.t_b_resize(img))))
-                    else:
-                        tmp_img = self.to_tensor(self.t_crop(self.t_b_resize(img)))
-                    img_list.append(tmp_img)
-                    label_list.append(int(label))
+        for row in file:
+            img_name,label = row.strip().split(' ')
+            self.img_list.append(img_name)
+            self.label_list.append(int(label)-1)
         file.close()
-        self.n_id = len(set(label_list))
+        self.n_id = len(set(self.label_list))
 
-        print('------End loading dataset... |    cost time: %.2f s------'%(time.time()-t0))
-        return torch.stack(img_list),torch.LongTensor(label_list)
-
+        #####################
+        tran = []
+        if crop == True:
+            tran.append(transforms.Resize((resize[0]+50,resize[1]+50)))
+            tran.append(transforms.RandomCrop(resize))
+        else:
+            tran.append(transforms.Resize(resize))
+        if flip == True:
+            tran.append(transforms.RandomHorizontalFlip())
+            
+        tran.append(transforms.ToTensor())
+        if pretrained_model == True:
+            tran.append(transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                              std=[0.229, 0.224, 0.225]))
+        self.compose = transforms.Compose(tran)
     def __getitem__(self,idx):
-        return {'img':self.data[idx],'gt':self.label[idx]}
+        img_name,label = self.img_list[idx],self.label_list[idx]
+        img = Image.open(img_name)
+        img = self.compose(img)
+        return {'img':img,'gt':torch.LongTensor([label])}
 
     def __len__(self):
-        return self.data.size(0)
+        return len(self.img_list)
             
 
 
-def Get_DataLoader(dataset,batch_size=128,shuffle=True):
-    return DataLoader(dataset,batch_size = batch_size,shuffle=True)
+def Get_DataLoader(dataset,batch_size=128,shuffle=True,num_workers=6):
+    return DataLoader(dataset,batch_size = batch_size,shuffle=True,num_workers=6)
 
 
 
+# def generate_txt():
+    # path = sys.argv[1]
+
+    # img = glob.glob(path)
+    # print(img)
+    # # exit(-1)
+    # file = open('train_info.txt','w')
+    # for i in range(len(img)):
+        # file.write(img[i]+' %d'%(i))
+        # file.write('\n')
 if __name__ == '__main__':
     
     pretrained = True
@@ -88,24 +77,17 @@ if __name__ == '__main__':
     print('n_batch:',len(D)//128+1)
 
     model = models.ResNet(D.n_id,n_layers=18,pretrained=pretrained).cuda()
+    
+
 
     for i,data in enumerate(loader):
         x = data['img']
         y = data['gt']
-
+        
         output = model(Variable(x).cuda())
         print(output.size())
-        break
     
 
 
 
-# def generate_txt():
-    # path = sys.argv[1]
-
-    # img = glob.glob(path)
-    # file = open('train_info.txt','w')
-    # for i in range(len(img)):
-        # file.write(img[i]+' %d'%(i))
-        # file.write('\n')
 
