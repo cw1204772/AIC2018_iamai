e216f472705c9e4b2c6d5472ed4090ebebc45ed9
cw1204772 <peter19940407@yahoo.com.tw>
2018-02-01 01:43:55 +0800

    add option generate more triplet samples

--- ReID/ReID_CNN/create_VeRi_triplet.py
+++ ReID/ReID_CNN/create_VeRi_triplet.py
@@ -6,18 +6,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Triplet generator for VeRi dataset')
     parser.add_argument('--db_txt', help='database txt')
     parser.add_argument('--output', help='the output txt file listing all triplets')
+    parser.add_argument('--k', type=int, default=1, help='how many times the triplet instance is to training images')
     args = parser.parse_args()
 
     db = np.loadtxt(args.db_txt, dtype=str)
     label = db[:, 1].astype(int)
     out = []
-    for i in range(label.shape[0]):
-        l = label[i]
-        pos = np.nonzero(label == l)
-        rng = np.random.permutation(pos[0])
-        pos = rng[1] if rng[0] == i else rng[0]
-        neg = np.nonzero(label != l)
-        rng = np.random.permutation(neg[0])
-        neg = rng[0]
-        out.append([i, pos, neg])
+    for k in range(args.k):
+        for i in range(label.shape[0]):
+            l = label[i]
+            pos = np.nonzero(label == l)
+            rng = np.random.permutation(pos[0])
+            pos = rng[1] if rng[0] == i else rng[0]
+            neg = np.nonzero(label != l)
+            rng = np.random.permutation(neg[0])
+            neg = rng[0]
+            out.append([i, pos, neg])
     np.savetxt(args.output, np.array(out, dtype=int), fmt='%d')
